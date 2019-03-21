54b10545765dfa96787be4194952d55629640da1
gongpengyang <gongpengyang@kaiyuan.net>
2019-03-21 14:01:06 +0800

    7

--- upload.sh
+++ upload.sh
@@ -1,1 +1,17 @@
-pod repo push PrivatePods A.podspec --verbose --allow-warnings
+#!/bin/bash
+
+VersionString=`grep -E 's.version.*=' A.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+
+NewVersionNumber=$(($VersionNumber + 1))
+LineNumber=`grep -nE 's.version.*=' A.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" A.podspec
+
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+
+git add .
+git commit -am ${NewVersionNumber}
+git tag ${NewVersionNumber}
+git push origin master --tags
+pod repo push Libraries A.podspec --verbose --allow-warnings --use-libraries
+
